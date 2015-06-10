376168850ad710899d2ae264ab1dba165ed5d5e7
Reinaldo Barcelos Mendes <reinaldorock@yahoo.com.br>
2015-06-10 01:24:58 -0300

    Update runner.rb
    fix: run on drb

--- lib/guard/test/runner.rb
+++ lib/guard/test/runner.rb
@@ -99,7 +99,7 @@ module Guard
 
       def includes_and_requires(paths)
         parts = []
-        parts << Array(options[:include]).map { |path| "-I\"#{path}\"" } unless zeus? || spring?
+        parts << Array(options[:include]).map { |path| "-I\"#{path}\"" } unless zeus? || spring? || drb?
         parts << paths if zeus? || spring?
         parts << '-r bundler/setup' if bundler?
         parts << '-r rubygems' if rubygems?
